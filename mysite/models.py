bccd0b66c16a5bf7ed1b245c4c15847577cf00bb
Tomvictor <vjtomvictor@icloud.com>
2016-07-25 18:53:21 +0530

    rebase

--- mysite/models.py
+++ mysite/models.py
@@ -1,23 +1,27 @@
 from django.db import models
 from django.utils import timezone
+
+
 # Create your models here.
 
 class Candidate(models.Model):
     FirstName = models.CharField(max_length=60)
-    LastName  = models.CharField(max_length=60)
+    LastName = models.CharField(max_length=60)
     GenderChoice = (
-        ('M','Male'),('F','Female'),('O','Other')
+        ('M', 'Male'), ('F', 'Female'), ('O', 'Other')
     )
-    Gender = models.CharField(max_length=10,choices=GenderChoice)
-    DateOfBirth = models.DateField(default=timezone.now())
+    Gender = models.CharField(max_length=10, choices=GenderChoice)
+    DateOfBirth = models.DateField(default=timezone.now().date())
     Mail = models.EmailField(null=True)
     Phone = models.CharField(max_length=20)
     Organisation = models.CharField(max_length=500)
     Address = models.TextField()
-    FromDate = models.DateField()
-    ToDate = models.DateField()
+    FromDate = models.DateField(default=timezone.now().date())
+    ToDate = models.DateField(default=timezone.now().date())
+    Notes = models.TextField(null=True)
     VerificationCode = models.CharField(max_length=180)
     ProfilePicture = models.FileField(upload_to="profile/%Y/%m/")
     Certificate = models.FileField(upload_to="certificate/%Y/%m/")
+
     def __str__(self):
         return self.FirstName
--- mysite/views.py
+++ mysite/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from mysite.models import Candidate
 
 # Create your views here.
 
 def Home(request):
-
-    return render(request,'home.html',{})
+    query = request.POST.get("q")
+    if query:
+        CandidateDetails = get_object_or_404(Candidate, VerificationCode = query)
+        return render(request,'candidate.html',{})
+    else:
+        return render(request, 'home.html', {})
