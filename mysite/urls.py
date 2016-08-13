477c1d9a6c1bfb840cc76d3ace382eed67380977
Tomvictor <vjtomvictor@icloud.com>
2016-08-13 15:47:13 +0530

    final production deployed

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from mysite import views
 
 urlpatterns = [
-    url(r'^$',views.Home,name='home-page')
+    url(r'^$',views.Home,name='home-page'),
+    url(r'^get/',views.Details,name='details')
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -1,13 +1,16 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from mysite.models import Candidate
+
 
 # Create your views here.
 
 def Home(request):
-    query = request.POST.get("q")
-    #print(query)
+    return render(request, 'home.html', {})
+
+
+def Details(request):
+    query = request.GET.get("q")
     if query:
-        CandidateDetails = get_object_or_404(Candidate, VerificationCode = query)
-        return render(request,'page.html',{"item":CandidateDetails})
-    else:
-        return render(request, 'home.html', {})
+        CandidateDetails = get_object_or_404(Candidate, VerificationCode=query)
+        return render(request, 'page.html', {"item": CandidateDetails})
+    return redirect("mysite:home-page")
